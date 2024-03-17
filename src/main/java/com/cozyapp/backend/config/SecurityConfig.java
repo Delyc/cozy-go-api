dc1093995016b9235109901cf918b4c202c49bc9
delyc <delyce35@gmail.com>
2024-03-17 19:14:48 +0200

    feat: adding welcome endpoint

--- src/main/java/com/cozyapp/backend/config/SecurityConfig.java
+++ src/main/java/com/cozyapp/backend/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
-                .authorizeHttpRequests(request -> request.requestMatchers("/auth/**", "/public/**").permitAll()
+                .authorizeHttpRequests(request -> request.requestMatchers("/auth/**", "/public/**", "/").permitAll()
                         .requestMatchers("/admin/**").hasAnyAuthority("ADMIN")
                         .requestMatchers("/user/**").hasAnyAuthority("USER")
                         .requestMatchers("/adminuser/**").hasAnyAuthority("USER", "ADMIN")
---
+++ src/main/java/com/cozyapp/backend/controller/Welcome.java
@@ -0,0 +1,13 @@
+package com.cozyapp.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+    
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to Cozy API";
+    }
+}
