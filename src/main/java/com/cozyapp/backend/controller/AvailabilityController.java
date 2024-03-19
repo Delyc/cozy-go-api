69c0ad09a5fa311889c1139bb9c5b48620392ab5
delyc <delyce35@gmail.com>
2024-03-19 22:39:19 +0200

    feat: updating availability

--- src/main/java/com/cozyapp/backend/controller/AvailabilityController.java
+++ src/main/java/com/cozyapp/backend/controller/AvailabilityController.java
@@ -1,9 +1,14 @@
 package com.cozyapp.backend.controller;
 
+import java.util.List;
+
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cozyapp.backend.dto.AvailabilityDto;
 import com.cozyapp.backend.entity.Availability;
+import com.cozyapp.backend.entity.House;
 import com.cozyapp.backend.service.AvailabilityService;
 
 @RestController
@@ -27,5 +33,30 @@ public class AvailabilityController {
         return new ResponseEntity<>(newAvailability, HttpStatus.CREATED);
     }
 
+     @DeleteMapping("agent/deleteAvailability/{id}")
+    public ResponseEntity<Void> deleteHouseById(@PathVariable Integer id) {
+        availabilityService.deleteAvailabilityById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/public/allAvailabilities")
+    public List<Availability> getAllHouses() {
+        return availabilityService.getAllAvailabilities();
+    }
+
+    @GetMapping("/public/availability/{id}")
+    public ResponseEntity<Availability> getHouseById(@PathVariable Integer id) {
+        return availabilityService.getHAvailabilityById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+     @PatchMapping("/agent/updateAvailability/{id}")
+    public ResponseEntity<Availability> updateAvailability(@PathVariable Integer id, @RequestBody Availability availabilityDetails) {
+        return availabilityService.updateAvailability(id, availabilityDetails)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
     
 }
--- src/main/java/com/cozyapp/backend/service/AvailabilityService.java
+++ src/main/java/com/cozyapp/backend/service/AvailabilityService.java
@@ -1,10 +1,14 @@
 package com.cozyapp.backend.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cozyapp.backend.dto.AvailabilityDto;
 import com.cozyapp.backend.entity.Availability;
+import com.cozyapp.backend.entity.House;
 import com.cozyapp.backend.entity.OurUsers;
 import com.cozyapp.backend.repository.AvailabilityRepo;
 import com.cozyapp.backend.repository.OurUserRepo;
@@ -20,7 +24,7 @@ public class AvailabilityService {
 
     public Availability addAvailability(AvailabilityDto availabilityDto, Integer userId) {
         OurUsers user = ourUserRepo.findById(userId)
-            .orElseThrow(() -> new RuntimeException("User not found")); // Customize exception as needed
+                .orElseThrow(() -> new RuntimeException("User not found")); // Customize exception as needed
 
         Availability availability = new Availability();
         availability.setStartTime(availabilityDto.getStartTime());
@@ -30,6 +34,35 @@ public class AvailabilityService {
 
         return availabilityRepo.save(availability);
     }
-    
-    
+
+    public List<Availability> getAllAvailabilities() {
+        return availabilityRepo.findAll();
+    }
+
+    public Optional<Availability> getHAvailabilityById(Integer id) {
+        return availabilityRepo.findById(id);
+    }
+
+    public void deleteAvailabilityById(Integer id) {
+        availabilityRepo.deleteById(id);
+    }
+
+
+    public Optional<Availability> updateAvailability(Integer id, Availability availabilityDetails) {
+        return availabilityRepo.findById(id).map(existingAvailability -> {
+            if (availabilityDetails.getEndTime() != null) {
+                existingAvailability.setEndTime(availabilityDetails.getEndTime());
+            }
+            if (availabilityDetails.getStartTime() != null) {
+                existingAvailability.setStartTime(availabilityDetails.getStartTime());
+            }
+
+            if(availabilityDetails.getStatus() != null) {
+                existingAvailability.setStatus(availabilityDetails.getStatus());
+            }
+
+            return availabilityRepo.save(existingAvailability);
+        });
+    }
+
 }
