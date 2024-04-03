81883aecac206d6c1f543f474150fd24456e2707
delyc <delyce35@gmail.com>
2024-04-04 01:06:18 +0200

    fix: fixing cors issue in auth controller

--- src/main/java/com/cozyapp/backend/controller/WishlistController.java
+++ src/main/java/com/cozyapp/backend/controller/WishlistController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.cozyapp.backend.service.WishlistService;
 
 @RestController
 @RequestMapping
+@CrossOrigin(origins = "http://localhost:3000")
 
 public class WishlistController {
 
