f409befb461a6493f0e359c88fb1bc4541b17185
delyc <delyce35@gmail.com>
2024-04-04 01:06:51 +0200

    fix: fixing cors issue in auth controller

--- src/main/java/com/cozyapp/backend/controller/AdminUsers.java
+++ src/main/java/com/cozyapp/backend/controller/AdminUsers.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,8 @@ import com.cozyapp.backend.entity.Product;
 import com.cozyapp.backend.repository.ProductRepo;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
+
 public class AdminUsers {
 
     @Autowired
